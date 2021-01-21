b311592df98bce556b0ade36d1f54d1e1498e0c5
Daniel TISCHER <dos.7182@gmail.com>
2021-01-21 17:07:37 +0100

    refactor test code

--- main_test.go
+++ main_test.go
@@ -8,43 +8,34 @@ import (
 func TestSimple(t *testing.T) {
 	actual := addSwitchIfNeeded("go", "e:\\src")
 	expected := "go"
-
-	if !strings.Contains(actual, expected) {
-		t.Errorf("Expected: %s, but was: %s", expected, actual)
-	}
+	compare(actual, expected, t)
 }
 
 func TestSame(t *testing.T) {
 	actual := addSwitchIfNeeded("e:\\go", "e:\\src")
 	expected := "e:\\go"
-
-	if !strings.Contains(actual, expected) {
-		t.Errorf("Expected: %s, but was: %s", expected, actual)
-	}
+	compare(actual, expected, t)
 }
 
 func TestDifferent(t *testing.T) {
 	actual := addSwitchIfNeeded("c:\\go", "e:\\src")
 	expected := "/d c:\\go"
-
-	if !strings.Contains(actual, expected) {
-		t.Errorf("Expected: %s, but was: %s", expected, actual)
-	}
+	compare(actual, expected, t)
 }
 
 func TestQuote(t *testing.T) {
 	actual := quote("c:\\Program Files")
 	expected := "\"c:\\Program Files\""
-
-	if !strings.Contains(actual, expected) {
-		t.Errorf("Expected: %s, but was: %s", expected, actual)
-	}
+	compare(actual, expected, t)
 }
 
 func TestNoQuote(t *testing.T) {
 	actual := quote("c:\\go")
 	expected := "c:\\go"
+	compare(actual, expected, t)
+}
 
+func compare(actual, expected string, t *testing.T) {
 	if !strings.Contains(actual, expected) {
 		t.Errorf("Expected: %s, but was: %s", expected, actual)
 	}
